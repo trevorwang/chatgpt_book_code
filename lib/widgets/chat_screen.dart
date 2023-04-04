613175058658af53d4780536b9fdffeec2401a9a
Trevor Wang <trevor.wang@qq.com>
2023-04-04 10:32:02 +0800

    消息列表和输入框

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -9,8 +9,47 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat'),
       ),
-      body: const Center(
-        child: Text('Chat Screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              // 聊天消息列表
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      const CircleAvatar(
+                        child: Text('A'),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text('Message $index'),
+                    ],
+                  );
+                },
+                itemCount: 5, // 消息数量
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 8,
+                ),
+              ),
+            ),
+            // 输入框
+            TextField(
+              decoration: InputDecoration(
+                  hintText: 'Type a message', // 显示在输入框内的提示文字
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      // 这里处理发送事件
+                    },
+                    icon: const Icon(
+                      Icons.send,
+                    ),
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
