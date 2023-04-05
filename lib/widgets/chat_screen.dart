bb0b20502fa45fafb1f7189e638774440d62b786
Trevor Wang <trevor.wang@qq.com>
2023-04-05 22:34:39 +0800

    flutter_hooks 重构用户输入组件

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -8,11 +8,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../states/messge_state.dart';
 
 class ChatScreen extends HookConsumerWidget {
-  final _textController = TextEditingController();
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final chatUIState = ref.watch(chatUiProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
@@ -20,69 +18,17 @@ class ChatScreen extends HookConsumerWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          children: [
-            const Expanded(
+          children: const [
+            Expanded(
               // 聊天消息列表
               child: ChatMessageList(),
             ),
             // 输入框
-            TextField(
-              enabled: !chatUIState.requestLoading,
-              controller: _textController,
-              decoration: InputDecoration(
-                  hintText: 'Type a message', // 显示在输入框内的提示文字
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      // 这里处理发送事件
-                      if (_textController.text.isNotEmpty) {
-                        _sendMessage(ref, _textController.text);
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.send,
-                    ),
-                  )),
-            ),
+            UserInputWidget(),
           ],
         ),
       ),
     );
-  }
-
-  // 增加WidgetRef
-  _sendMessage(WidgetRef ref, String content) {
-    final message = Message(
-      id: uuid.v4(),
-      content: content,
-      isUser: true,
-      timestamp: DateTime.now(),
-    );
-    ref.read(messageProvider.notifier).upsertMessage(message); // 添加消息
-    _textController.clear();
-    _requestChatGPT(ref, content);
-  }
-
-  _requestChatGPT(WidgetRef ref, String content) async {
-    ref.read(chatUiProvider.notifier).setRequestLoading(true);
-    try {
-      final id = uuid.v4();
-      await chatgpt.streamChat(
-        content,
-        onSuccess: (text) {
-          final message = Message(
-            id: id,
-            content: text,
-            isUser: false,
-            timestamp: DateTime.now(),
-          );
-          ref.read(messageProvider.notifier).upsertMessage(message);
-        },
-      );
-    } catch (err) {
-      logger.e("requestChatGPT error: $err", err);
-    } finally {
-      ref.read(chatUiProvider.notifier).setRequestLoading(false);
-    }
   }
 }
 
@@ -113,6 +59,70 @@ class ChatMessageList extends HookConsumerWidget {
         height: 16,
       ),
     );
+  }
+}
+
+class UserInputWidget extends HookConsumerWidget {
+  const UserInputWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final chatUIState = ref.watch(chatUiProvider);
+    final controller = useTextEditingController();
+    return TextField(
+      enabled: !chatUIState.requestLoading,
+      controller: controller,
+      decoration: InputDecoration(
+          hintText: 'Type a message', // 显示在输入框内的提示文字
+          suffixIcon: IconButton(
+            onPressed: () {
+              // 这里处理发送事件
+              if (controller.text.isNotEmpty) {
+                _sendMessage(ref, controller);
+              }
+            },
+            icon: const Icon(
+              Icons.send,
+            ),
+          )),
+    );
+  }
+
+  // 增加WidgetRef
+  _sendMessage(WidgetRef ref, TextEditingController controller) async {
+    final content = controller.text;
+    final message = Message(
+      id: uuid.v4(),
+      content: content,
+      isUser: true,
+      timestamp: DateTime.now(),
+    );
+    ref.read(messageProvider.notifier).upsertMessage(message); // 添加消息
+    controller.clear();
+    _requestChatGPT(ref, content);
+  }
+
+  _requestChatGPT(WidgetRef ref, String content) async {
+    ref.read(chatUiProvider.notifier).setRequestLoading(true);
+    try {
+      final id = uuid.v4();
+      await chatgpt.streamChat(
+        content,
+        onSuccess: (text) {
+          final message = Message(
+            id: id,
+            content: text,
+            isUser: false,
+            timestamp: DateTime.now(),
+          );
+          ref.read(messageProvider.notifier).upsertMessage(message);
+        },
+      );
+    } catch (err) {
+      logger.e("requestChatGPT error: $err", err);
+    } finally {
+      ref.read(chatUiProvider.notifier).setRequestLoading(false);
+    }
   }
 }
 
