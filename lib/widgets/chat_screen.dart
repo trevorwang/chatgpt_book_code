7d54b2382a33b0c052ada619b08a22f4edbca7bd
Trevor Wang <trevor.wang@qq.com>
2023-04-07 21:21:53 +0800

    fixup! markdown 消息支持

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -5,7 +5,6 @@ import 'package:chatgpt/states/chat_ui_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:markdown_widget/config/all.dart';
 import 'package:markdown_widget/markdown_widget.dart';
 
 import '../markdown/code_wrapper.dart';
@@ -185,19 +184,21 @@ class MessageContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: MarkdownGenerator(
-        config: MarkdownConfig().copy(configs: [
-          const PreConfig().copy(wrapper: codeWrapper),
-        ]),
-        generators: [
-          latexGenerator,
-        ],
-        inlineSyntaxes: [
-          LatexSyntax(),
-        ],
-      ).buildWidgets(message.content),
+    return SelectionArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: MarkdownGenerator(
+          config: MarkdownConfig().copy(configs: [
+            const PreConfig().copy(wrapper: codeWrapper),
+          ]),
+          generators: [
+            latexGenerator,
+          ],
+          inlineSyntaxes: [
+            LatexSyntax(),
+          ],
+        ).buildWidgets(message.content),
+      ),
     );
   }
 }
