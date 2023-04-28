0f4ac8093484b9773a54c6b7059e4ad0e85a40c9
Trevor Wang <trevor.wang@qq.com>
2023-04-28 19:51:35 +0800

    refator 用户输入

---
+++ lib/widgets/chat_input.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../injection.dart';
+import '../models/message.dart';
+import '../models/session.dart';
+import '../states/chat_ui_state.dart';
+import '../states/message_state.dart';
+import '../states/session_state.dart';
+
+class UserInputWidget extends HookConsumerWidget {
+  const UserInputWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useTextEditingController();
+    final uiState = ref.watch(chatUiProvider);
+    return TextField(
+      enabled: !uiState.requestLoading,
+      controller: controller,
+      decoration: InputDecoration(
+          hintText: 'Type a message', // 显示在输入框内的提示文字
+          suffixIcon: IconButton(
+            onPressed: () {
+              // 这里处理发送事件
+              if (controller.text.isNotEmpty) {
+                _sendMessage(ref, controller);
+              }
+            },
+            icon: const Icon(
+              Icons.send,
+            ),
+          )),
+    );
+  }
+
+  // 增加WidgetRef
+  _sendMessage(WidgetRef ref, TextEditingController controller) async {
+    final content = controller.text;
+    Message message = _createMessage(content);
+
+    var active = ref.watch(activeSessionProvider);
+    var sessionId = active?.id ?? 0;
+    if (sessionId <= 0) {
+      active = Session(title: content);
+      // final id = await db.sessionDao.upsertSession(active);
+      active = await ref
+          .read(sessionStateNotifierProvider.notifier)
+          .upsertSesion(active);
+      sessionId = active.id!;
+      ref
+          .read(sessionStateNotifierProvider.notifier)
+          .setActiveSession(active.copyWith(id: sessionId));
+    }
+
+    ref.read(messageProvider.notifier).upsertMessage(
+          message.copyWith(sessionId: sessionId),
+        ); // 添加消息
+    controller.clear();
+    _requestChatGPT(ref, content, sessionId: sessionId);
+  }
+
+  Message _createMessage(
+    String content, {
+    String? id,
+    bool isUser = true,
+    int? sessionId,
+  }) {
+    final message = Message(
+      id: id ?? uuid.v4(),
+      content: content,
+      isUser: isUser,
+      timestamp: DateTime.now(),
+      sessionId: sessionId ?? 0,
+    );
+    return message;
+  }
+
+  _requestChatGPT(
+    WidgetRef ref,
+    String content, {
+    int? sessionId,
+  }) async {
+    ref.read(chatUiProvider.notifier).setRequestLoading(true);
+    try {
+      final id = uuid.v4();
+      await chatgpt.streamChat(
+        content,
+        onSuccess: (text) {
+          final message =
+              _createMessage(text, id: id, isUser: false, sessionId: sessionId);
+          ref.read(messageProvider.notifier).upsertMessage(message);
+        },
+      );
+    } catch (err) {
+      logger.e("requestChatGPT error: $err", err);
+    } finally {
+      ref.read(chatUiProvider.notifier).setRequestLoading(false);
+    }
+  }
+}
--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -5,13 +5,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:markdown_widget/markdown_widget.dart';
 
 import '../markdown/code_wrapper.dart';
-import '../models/session.dart';
 import '../states/message_state.dart';
-import '../injection.dart';
 import '../markdown/latex.dart';
 import '../models/message.dart';
-import '../states/chat_ui_state.dart';
 import '../states/session_state.dart';
+import 'chat_input.dart';
 
 class ChatScreen extends HookConsumerWidget {
   const ChatScreen({super.key});
@@ -81,98 +79,6 @@ class ChatMessageList extends HookConsumerWidget {
         height: 16,
       ),
     );
-  }
-}
-
-class UserInputWidget extends HookConsumerWidget {
-  const UserInputWidget({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final chatUIState = ref.watch(chatUiProvider);
-    final controller = useTextEditingController();
-    return TextField(
-      enabled: !chatUIState.requestLoading,
-      controller: controller,
-      decoration: InputDecoration(
-          hintText: 'Type a message', // 显示在输入框内的提示文字
-          suffixIcon: IconButton(
-            onPressed: () {
-              // 这里处理发送事件
-              if (controller.text.isNotEmpty) {
-                _sendMessage(ref, controller);
-              }
-            },
-            icon: const Icon(
-              Icons.send,
-            ),
-          )),
-    );
-  }
-
-  // 增加WidgetRef
-  _sendMessage(WidgetRef ref, TextEditingController controller) async {
-    final content = controller.text;
-    Message message = _createMessage(content);
-
-    var active = ref.watch(activeSessionProvider);
-    var sessionId = active?.id ?? 0;
-    if (sessionId <= 0) {
-      active = Session(title: content);
-      // final id = await db.sessionDao.upsertSession(active);
-      active = await ref
-          .read(sessionStateNotifierProvider.notifier)
-          .upsertSesion(active);
-      sessionId = active.id!;
-      ref
-          .read(sessionStateNotifierProvider.notifier)
-          .setActiveSession(active.copyWith(id: sessionId));
-    }
-
-    ref.read(messageProvider.notifier).upsertMessage(
-          message.copyWith(sessionId: sessionId),
-        ); // 添加消息
-    controller.clear();
-    _requestChatGPT(ref, content, sessionId: sessionId);
-  }
-
-  Message _createMessage(
-    String content, {
-    String? id,
-    bool isUser = true,
-    int? sessionId,
-  }) {
-    final message = Message(
-      id: id ?? uuid.v4(),
-      content: content,
-      isUser: isUser,
-      timestamp: DateTime.now(),
-      sessionId: sessionId ?? 0,
-    );
-    return message;
-  }
-
-  _requestChatGPT(
-    WidgetRef ref,
-    String content, {
-    int? sessionId,
-  }) async {
-    ref.read(chatUiProvider.notifier).setRequestLoading(true);
-    try {
-      final id = uuid.v4();
-      await chatgpt.streamChat(
-        content,
-        onSuccess: (text) {
-          final message =
-              _createMessage(text, id: id, isUser: false, sessionId: sessionId);
-          ref.read(messageProvider.notifier).upsertMessage(message);
-        },
-      );
-    } catch (err) {
-      logger.e("requestChatGPT error: $err", err);
-    } finally {
-      ref.read(chatUiProvider.notifier).setRequestLoading(false);
-    }
   }
 }
 
