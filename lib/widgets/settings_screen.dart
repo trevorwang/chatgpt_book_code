2e2cbb2e1e04b9911385b449eb082e2d844578ba
Trevor Wang <trevor.wang@qq.com>
2023-05-17 15:22:15 +0800

    国际化设置页面

--- lib/widgets/settings_screen.dart
+++ lib/widgets/settings_screen.dart
@@ -24,112 +24,177 @@ class SettingsWindow extends HookConsumerWidget {
   const SettingsWindow({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final items = ref.watch(settingListProvider);
-    final apptheme =
-        ref.watch(settingStateProvider).valueOrNull?.appTheme ?? AppTheme.auto;
-    final controller = useTextEditingController();
-
-    return ListView.separated(
-      itemBuilder: (context, index) {
-        final item = items[index];
-
-        if (index == 3) {
-          return ListTile(
-            title: Text(item.title),
-            subtitle: Row(
-              children: [
-                RadioMenuButton(
-                  value: AppTheme.auto,
-                  groupValue: apptheme,
-                  onChanged: (v) {
-                    ref
-                        .read(settingStateProvider.notifier)
-                        .setAppTheme(AppTheme.auto);
-                  },
-                  child: Text(AppIntl.of(context).themeSystem),
-                ),
-                RadioMenuButton(
-                  value: AppTheme.light,
-                  groupValue: apptheme,
-                  onChanged: (v) {
-                    ref
-                        .read(settingStateProvider.notifier)
-                        .setAppTheme(AppTheme.light);
-                  },
-                  child: Text(AppIntl.of(context).themeLight),
-                ),
-                RadioMenuButton(
-                  value: AppTheme.dark,
-                  groupValue: apptheme,
-                  onChanged: (v) {
-                    ref
-                        .read(settingStateProvider.notifier)
-                        .setAppTheme(AppTheme.dark);
-                  },
-                  child: Text(AppIntl.of(context).themeDark),
-                ),
-              ],
-            ),
-          );
-        }
-        return ListTile(
-          title: Text(item.title),
-          subtitle:
-              Text(item.subtitle ?? AppIntl.of(context).unkonwSettingLabel),
-          trailing: const Icon(Icons.arrow_forward_ios),
-          onTap: () async {
-            final text = await showEditor(controller, item, ref);
-            if (text == null) return;
-            switch (item.key) {
-              case SettingKey.apiKey:
-                ref.read(settingStateProvider.notifier).setApiKey(text);
-                break;
-              case SettingKey.httpProxy:
-                ref.read(settingStateProvider.notifier).setHttpProxy(text);
-                break;
-              case SettingKey.baseUrl:
-                ref.read(settingStateProvider.notifier).setBaseUrl(text);
-                break;
-              default:
-                break;
-            }
-          },
-        );
-      },
-      separatorBuilder: (context, index) => const Divider(),
-      itemCount: items.length,
+    return ListView(
+      children: const [
+        SettingItemApiKey(),
+        Divider(),
+        SettingItemHttpProxy(),
+        Divider(),
+        SettingItemOpenAiBase(),
+        Divider(),
+        SettingItemAppTheme(),
+      ],
     );
   }
+}
 
-  Future<String?> showEditor(
-      TextEditingController controller, SettingItem item, WidgetRef ref) async {
-    controller.text = item.subtitle ?? '';
-    return await showDialog<String?>(
-      context: ref.context,
-      builder: (context) => AlertDialog(
-        actions: [
-          // negitive button
-          TextButton(
-            child: Text(AppIntl.of(context).cancel),
-            onPressed: () {
-              Navigator.of(context).pop();
+class SettingItemHttpProxy extends HookConsumerWidget {
+  const SettingItemHttpProxy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final title = AppIntl.of(context).settingsHttpProxyTitle;
+    final settings = ref.watch(settingStateProvider).valueOrNull;
+    final controller = useTextEditingController();
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(settings?.httpProxy ?? ""),
+      onTap: () async {
+        final text = await showEditor(
+          controller,
+          ref,
+          title,
+          text: settings?.httpProxy,
+        );
+        if (text == null) return;
+        ref.read(settingStateProvider.notifier).setHttpProxy(text);
+      },
+    );
+  }
+}
+
+class SettingItemApiKey extends HookConsumerWidget {
+  const SettingItemApiKey({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final title = AppIntl.of(context).settingsApiKeyLabel;
+    final settings = ref.watch(settingStateProvider).valueOrNull;
+    final controller = useTextEditingController();
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(settings?.apiKey ?? ""),
+      onTap: () async {
+        final text = await showEditor(
+          controller,
+          ref,
+          title,
+          text: settings?.apiKey,
+        );
+        if (text == null) return;
+        ref.read(settingStateProvider.notifier).setApiKey(text);
+      },
+    );
+  }
+}
+
+class SettingItemOpenAiBase extends HookConsumerWidget {
+  const SettingItemOpenAiBase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final title = AppIntl.of(context).settingsOpenaiApiBase;
+    final settings = ref.watch(settingStateProvider).valueOrNull;
+    final controller = useTextEditingController();
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(settings?.baseUrl ?? ""),
+      onTap: () async {
+        final text = await showEditor(
+          controller,
+          ref,
+          title,
+          text: settings?.baseUrl,
+        );
+        if (text == null) return;
+        ref.read(settingStateProvider.notifier).setBaseUrl(text);
+      },
+    );
+  }
+}
+
+class SettingItemAppTheme extends HookConsumerWidget {
+  const SettingItemAppTheme({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final apptheme =
+        ref.watch(settingStateProvider).valueOrNull?.appTheme ?? AppTheme.auto;
+    return ListTile(
+      title: Text(AppIntl.of(context).settingThemeLabel),
+      subtitle: Row(
+        children: [
+          RadioMenuButton(
+            value: AppTheme.auto,
+            groupValue: apptheme,
+            onChanged: (v) {
+              ref
+                  .read(settingStateProvider.notifier)
+                  .setAppTheme(AppTheme.auto);
             },
+            child: Text(AppIntl.of(context).themeSystem),
           ),
-          TextButton(
-            child: Text(AppIntl.of(context).ok),
-            onPressed: () {
-              final text = controller.text;
-              controller.clear();
-              Navigator.of(context).pop(text);
+          RadioMenuButton(
+            value: AppTheme.light,
+            groupValue: apptheme,
+            onChanged: (v) {
+              ref
+                  .read(settingStateProvider.notifier)
+                  .setAppTheme(AppTheme.light);
             },
+            child: Text(AppIntl.of(context).themeLight),
+          ),
+          RadioMenuButton(
+            value: AppTheme.dark,
+            groupValue: apptheme,
+            onChanged: (v) {
+              ref
+                  .read(settingStateProvider.notifier)
+                  .setAppTheme(AppTheme.dark);
+            },
+            child: Text(AppIntl.of(context).themeDark),
           ),
         ],
-        title: Text(item.title),
-        content: TextField(
-          controller: controller,
-          decoration: InputDecoration(hintText: item.hint),
-        ),
       ),
     );
   }
+}
+
+Future<String?> showEditor(
+  TextEditingController controller,
+  WidgetRef ref,
+  String title, {
+  String? text,
+  String? hint,
+}) async {
+  controller.text = text ?? '';
+  return await showDialog<String?>(
+    context: ref.context,
+    builder: (context) => AlertDialog(
+      actions: [
+        // negitive button
+        TextButton(
+          child: Text(AppIntl.of(context).cancel),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: Text(AppIntl.of(context).ok),
+          onPressed: () {
+            final text = controller.text;
+            controller.clear();
+            Navigator.of(context).pop(text);
+          },
+        ),
+      ],
+      title: Text(title),
+      content: TextField(
+        controller: controller,
+        decoration: InputDecoration(hintText: hint),
+      ),
+    ),
+  );
 }
